47a327eff114e38eebb7ee6c533937af748798a2
golujha9936 <golujha993465@gmail.com>
2023-04-11 21:42:35 +0530

    Update Soundcloud.py

--- StrangerMusic/platforms/Soundcloud.py
+++ StrangerMusic/platforms/Soundcloud.py
@@ -3,7 +3,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from StrangerMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
